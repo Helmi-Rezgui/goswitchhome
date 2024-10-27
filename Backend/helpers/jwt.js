4abe658b5eea774131645e25b6b45ab4630df6be
Helmi-Rezgui <helmiielrezgui@gmail.com>
2024-10-27 15:27:34 +0100

    first commit

---
+++ Backend/helpers/jwt.js
@@ -0,0 +1,37 @@
+const { expressjwt: jwt } = require("express-jwt");
+
+
+function authJwt() {
+  const secret = process.env.secret;
+  const api = process.env.API_URL;
+  return jwt({
+    secret,
+    algorithms: ["HS256"],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+    
+      `${api}/users/login`,
+      `${api}/users/register`,
+      `${api}/users/verify-email`,
+      `${api}/users/reset-password`,
+      `${api}/users/forgot-password`,
+      
+      
+
+    ],
+  });
+}
+
+async function isRevoked(req, jwt) {
+  const payload = jwt.payload;
+  
+  if (payload.role === "Admin") {
+    
+    return false;
+  }
+
+ 
+}
+
+module.exports = authJwt;
